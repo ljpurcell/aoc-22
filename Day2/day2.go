68e023226db20d568a385707a75ae9ef9105c973
Lyndon Purcell <lyndonpurcell23@gmail.com>
2023-11-28 08:48:02 +1100

    refactor day 2 part 1

--- Day2/day2.go
+++ Day2/day2.go
@@ -5,7 +5,6 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"strings"
 )
 
 var data = "Day2/day2_data.txt"
@@ -18,34 +17,25 @@ func DayTwoPartOne() {
 	scanner := bufio.NewScanner(file)
 	scanner.Split(bufio.ScanLines)
 
+    // A : X - Rock
+    // B : Y - Paper
+    // C : Z - Scissors
+
+    scores := map[string]int{
+        "B X": 1,
+        "C Y": 2,
+        "A Z": 3,
+        "A X": 4,
+        "B Y": 5,
+        "C Z": 6,
+        "C X": 7,
+        "A Y": 8,
+        "B Z": 9,
+    }
+
 	total := 0
 	for scanner.Scan() {
-		signs := strings.Split(scanner.Text(), " ")
-		opponent := signs[0]
-		suggested := signs[1]
-
-		if suggested == "X" {
-			total += 1
-			if opponent == "A" {
-				total += 3
-			} else if opponent == "C" {
-				total += 6
-			}
-		} else if suggested == "Y" {
-			total += 2
-			if opponent == "B" {
-				total += 3
-			} else if opponent == "A" {
-				total += 6
-			}
-		} else if suggested == "Z" {
-			total += 3
-			if opponent == "C" {
-				total += 3
-			} else if opponent == "B" {
-				total += 6
-			}
-		}
+        total += scores[scanner.Text()]
 	}
 
 	fmt.Println(total)
