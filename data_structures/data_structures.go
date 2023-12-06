cea109f77a5be4a7605e2e575a48048bee724546
Lyndon Purcell <lyndonpurcell23@gmail.com>
2023-12-07 03:54:32 +1100

    finished implementing stack and queue using container interface

--- data_structures/data_structures.go
+++ data_structures/data_structures.go
@@ -1,7 +1,29 @@
 package data_structures
 
-type Stack struct {
+type Container interface {
+	Contains(item string) bool
+}
+
+type containerImpl struct {
 	items []string
+}
+
+func (c *containerImpl) Contains(item string) bool {
+	for _, i := range c.items {
+		if i == item {
+			return true
+		}
+	}
+
+	return false
+}
+
+type Stack struct {
+	containerImpl
+}
+
+func NewStack() *Stack {
+	return &Stack{}
 }
 
 func (s *Stack) Push(item string) {
@@ -19,10 +41,36 @@ func (s *Stack) Pop() string {
 	return item
 }
 
-func NewStack() *Stack {
-    return &Stack{}
+func (s *Stack) Size() int {
+	return len(s.items)
 }
 
-func (s *Stack) Size() int {
-    return len(s.items)
+type Queue struct {
+	containerImpl
+}
+
+func NewQueue() *Queue {
+	return &Queue{}
+}
+
+func (q *Queue) Items() []string {
+    return q.items
+}
+
+func (q *Queue) Enqueue(item string) {
+    q.items = append(q.items, item)
+}
+
+func (q *Queue) Dequeue() string {
+    item := ""
+    if len(q.items) > 0 {
+        item = q.items[0]
+        q.items = q.items[1:]
+    }
+
+    return item
+}
+
+func (q *Queue) Size() int {
+	return len(q.items)
 }
